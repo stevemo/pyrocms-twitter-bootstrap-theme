e30ff56e6c6eff81e511bf9452368d5f4555032c
stevemo <stevemo2003@gmail.com>
2012-05-30 11:10:41 +0800

    created theme.php

---
+++ theme.php
@@ -0,0 +1,13 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Theme_Pyrobootstrap extends Theme
+{
+    public $name			= 'PyroCMS Twitter Bootstrap';
+    public $author			= 'Steve Montambeault';
+    public $author_website	= 'http://stevemo.ca/';
+    public $website			= 'http://twitter.github.com/bootstrap/index.html';
+    public $description		= 'Simple and flexible HTML, CSS, and Javascript for popular user interface components and interactions.';
+    public $version			= '0.1';
+}
+
+/* End of file theme.php */
